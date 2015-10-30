b45896f6f813f91e686f3f17dc98005d6562e28f
Zachary Weisman <zcweisman@gmail.com>
2015-10-29 21:13:57 -0700

    Fixed style on camerafeed.cpp

--- practice_tools/camerafeed.cpp
+++ practice_tools/camerafeed.cpp
@@ -8,38 +8,34 @@ using namespace std;
 using namespace cv;
 
 
-int main( int argc, const char** argv )
-{
+int main( int argc, const char** argv ) {
     CvCapture* capture = 0;
     Mat frame, frameCopy, image;
 
     capture = cvCaptureFromCAM( 0 ); //0=default, -1=any camera, 1..99=your camera
-    if(!capture) cout << "No camera detected" << endl;
+    if (!capture) cout << "No camera detected" << endl;
 
-    cvNamedWindow( "result", 1 );
+    cvNamedWindow("result", 1);
 
-    if( capture )
-    {
+    if (capture) {
         cout << "In capture ..." << endl;
-        for(;;)
-        {
-            IplImage* iplImg = cvQueryFrame( capture );
+        for(;;) {
+            IplImage* iplImg = cvQueryFrame(capture);
             frame = iplImg;
-            if( frame.empty() )
-                break;
-            if( iplImg->origin == IPL_ORIGIN_TL )
-                frame.copyTo( frameCopy );
-            else
-                flip( frame, frameCopy, 0 );
 
-            if( waitKey( 10 ) >= 0 )
+            if (frame.empty()) break;
+            if (iplImg->origin == IPL_ORIGIN_TL)
+                frame.copyTo( frameCopy );
+            else flip( frame, frameCopy, 0 );
+
+            if(waitKey( 10 ) >= 0)
                 cvReleaseCapture( &capture );
         }
 
         waitKey(0);
 
-    cvDestroyWindow("result");
+    	cvDestroyWindow("result");
 
-    return 0;
+    	return 0;
     }
 }
